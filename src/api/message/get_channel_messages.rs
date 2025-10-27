e3fcc6fea405831a5739f6b8fedd942faff24499
aerawi <aerawi@lemissa.com>
2025-10-27 16:11:45 +0100

    [ADD] src/api/message/get_channel_messages.rs

---
+++ src/api/message/get_channel_messages.rs
@@ -0,0 +1,47 @@
+use reqwest::Client;
+
+use crate::Message;
+
+pub async fn get_channel_messages(
+    channel_id: &str,
+    token: &str,
+    around: Option<String>,
+    before: Option<String>,
+    after: Option<String>,
+    limit: Option<String>,
+) -> Result<Vec<Message>, Box<dyn std::error::Error>> {
+    let mut api_url = format!("https://discord.com/api/v10/channels/{channel_id}/messages?");
+
+    if let Some(around) = around {
+        api_url.push_str(format!("around={}&", around.as_str()).as_str());
+    }
+    if let Some(before) = before {
+        api_url.push_str(format!("before={}&", before.as_str()).as_str());
+    }
+    if let Some(after) = after {
+        api_url.push_str(format!("after={}&", after.as_str()).as_str());
+    }
+    if let Some(limit) = limit {
+        api_url.push_str(format!("limit={}", limit.as_str()).as_str());
+    }
+
+    let client = Client::new();
+
+    let response = client
+        .get(&api_url)
+        .header("Authorization", token)
+        .send()
+        .await?;
+
+    if !response.status().is_success() {
+        eprintln!("API Error: Status code {}", response.status());
+        eprintln!("Body response: {}", response.text().await?);
+        return Err("Failed to request Discord API".into());
+    }
+
+    let mut messages: Vec<Message> = response.json().await?;
+
+    messages.reverse();
+
+    Ok(messages)
+}
