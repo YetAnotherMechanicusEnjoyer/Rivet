937eda66da74dbeea4a01305a2dd4461dd251d2f
aerawi <aerawi@lemissa.com>
2025-10-27 17:10:08 +0100

    [UPD] main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
-use serde::Deserialize;
 use std::{env, process};
 
-use crate::api::message::get_channel_messages::get_channel_messages;
+use crate::{api::message::get_channel_messages::get_channel_messages, model::channel::Message};
 
 pub mod api;
+pub mod model;
 
-#[derive(Debug, Deserialize)]
-struct Author {
-    username: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Message {
-    content: Option<String>,
-    author: Author,
-}
+pub type Error = Box<dyn std::error::Error>;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Error> {
     dotenvy::dotenv().ok();
-    let default_limit = env::var("DEFAULT_LIMIT").unwrap_or("50".into());
     const ENV_TOKEN: &str = "DISCORD_TOKEN";
     const ENV_CHANNEL: &str = "DISCORD_CHANNEL";
 
     let args: Vec<String> = env::args().collect();
-    let limit = args
-        .get(1)
-        .map(|s| s.as_str())
-        .unwrap_or(default_limit.as_str());
+    let limit = args.get(1);
 
     let channel_id = args.get(2).map(|s| s.as_str()).unwrap_or_else(|| {
         env::var(ENV_CHANNEL)
@@ -45,7 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let messages: Vec<Message> =
-        get_channel_messages(channel_id, token, None, None, None, Some(limit.to_string())).await?;
+        get_channel_messages(channel_id, token, None, None, None, limit.cloned()).await?;
 
     messages.iter().for_each(|msg| {
         let content = msg
